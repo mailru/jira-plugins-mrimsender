d424d247b21f964173f3b31b5d0e00eaa62e95f5
Firelord <a.magomedov@corp.mail.ru>
2015-05-13 20:30:53 +0300

    [mrimsender] check permission before send message

--- src/main/java/ru/mail/jira/plugins/mrimsender/protocol/MrimsenderEventListener.java
+++ src/main/java/ru/mail/jira/plugins/mrimsender/protocol/MrimsenderEventListener.java
@@ -5,8 +5,14 @@ import com.atlassian.event.api.EventListener;
 import com.atlassian.event.api.EventPublisher;
 import com.atlassian.jira.event.issue.IssueEvent;
 import com.atlassian.jira.event.issue.MentionIssueEvent;
+import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.notification.NotificationRecipient;
 import com.atlassian.jira.notification.NotificationSchemeManager;
+import com.atlassian.jira.permission.ProjectPermissions;
+import com.atlassian.jira.security.PermissionManager;
+import com.atlassian.jira.security.groups.GroupManager;
+import com.atlassian.jira.security.roles.ProjectRole;
+import com.atlassian.jira.security.roles.ProjectRoleManager;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.user.ApplicationUsers;
 import org.apache.commons.lang3.StringUtils;
@@ -16,22 +22,26 @@ import org.springframework.beans.factory.InitializingBean;
 import ru.mail.jira.plugins.mrimsender.configuration.PluginData;
 import ru.mail.jira.plugins.mrimsender.configuration.UserData;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class MrimsenderEventListener implements InitializingBean, DisposableBean {
     private static final Logger log = Logger.getLogger(MrimsenderEventListener.class);
 
     private final EventPublisher eventPublisher;
+    private final GroupManager groupManager;
     private final NotificationSchemeManager notificationSchemeManager;
+    private final PermissionManager permissionManager;
     private final PluginData pluginData;
+    private final ProjectRoleManager projectRoleManager;
     private final UserData userData = new UserData();
 
-    public MrimsenderEventListener(EventPublisher eventPublisher, NotificationSchemeManager notificationSchemeManager, PluginData pluginData) {
+    public MrimsenderEventListener(EventPublisher eventPublisher, GroupManager groupManager, NotificationSchemeManager notificationSchemeManager, PermissionManager permissionManager, PluginData pluginData, ProjectRoleManager projectRoleManager) {
         this.eventPublisher = eventPublisher;
+        this.groupManager = groupManager;
         this.notificationSchemeManager = notificationSchemeManager;
+        this.permissionManager = permissionManager;
         this.pluginData = pluginData;
+        this.projectRoleManager = projectRoleManager;
     }
 
     @Override
@@ -67,14 +77,35 @@ public class MrimsenderEventListener implements InitializingBean, DisposableBean
     public void onIssueEvent(IssueEvent issueEvent) {
         try {
             if (issueEvent.isSendMail()) {
-                List<ApplicationUser> recipients = new ArrayList<ApplicationUser>();
+                Set<ApplicationUser> recipients = new HashSet<ApplicationUser>();
+
                 for (NotificationRecipient notificationRecipient : notificationSchemeManager.getRecipients(issueEvent))
                     recipients.add(notificationRecipient.getUser());
+
+                if (issueEvent.getWorklog() != null)
+                    recipients = getFilteredRecipients(recipients, issueEvent.getWorklog().getRoleLevel(), issueEvent.getWorklog().getGroupLevel(), issueEvent.getIssue());
+                else if (issueEvent.getComment() != null)
+                    recipients = getFilteredRecipients(recipients, issueEvent.getComment().getRoleLevel(), issueEvent.getComment().getGroupLevel(), issueEvent.getIssue());
+
                 sendMessage(recipients, issueEvent);
             }
         } catch (Exception e) {
             log.error(e);
         }
+    }
+
+    private Set<ApplicationUser> getFilteredRecipients(Collection<ApplicationUser> recipients, ProjectRole projectRole, String groupName, Issue issue) {
+        Set<ApplicationUser> result = new HashSet<ApplicationUser>();
+        for (ApplicationUser user : recipients) {
+            if (!permissionManager.hasPermission(ProjectPermissions.BROWSE_PROJECTS, issue, user))
+                 continue;
+            if (groupName != null && !groupManager.isUserInGroup(user.getName(), groupName))
+                continue;
+            if (projectRole != null && !projectRoleManager.isUserInProjectRole(user, projectRole, issue.getProjectObject()))
+                continue;
+            result.add(user);
+        }
+        return result;
     }
 
     @SuppressWarnings("unused")
