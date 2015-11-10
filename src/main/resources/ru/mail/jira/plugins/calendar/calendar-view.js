56677b0c7eda82620b139feb4df8ccd602be94fa
Firelord <a.magomedov@corp.mail.ru>
2015-11-10 14:04:16 +0300

    add top.mail.ru counter

--- src/main/resources/ru/mail/jira/plugins/calendar/calendar-view.js
+++ src/main/resources/ru/mail/jira/plugins/calendar/calendar-view.js
@@ -1,5 +1,7 @@
 (function ($) {
     AJS.toInit(function () {
+        runTopMailScript();
+
         var $calendar = $('#calendar-full-calendar');
         var fullCalendarInstance;
         var sharedCounter;
@@ -966,3 +968,15 @@
         }
     });
 })(AJS.$);
+
+function runTopMailScript() {
+    var _tmr = window._tmr || (window._tmr = []);
+    _tmr.push({id: "2706504", type: "pageView", start: (new Date()).getTime()});
+    (function (d, w, id) {
+        if (d.getElementById(id)) return;
+        var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+        ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
+        var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+        if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+    })(document, window, "topmailru-code");
+}
