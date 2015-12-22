78c3628fe6396e9bfe9c46c22029212355ddb2b7
Ivan Mashintsev <i.mashintsev@corp.mail.ru>
2015-12-22 13:35:04 +0300

    Update to vis-timeline library. Highlight weekends in timeline view.

--- src/main/resources/ru/mail/jira/plugins/calendar/timeline-view.js
+++ src/main/resources/ru/mail/jira/plugins/calendar/timeline-view.js
@@ -15,58 +15,58 @@
         var TimelineView;
         var issueInfoTpl = _.template($('#issue-info-template').html());
 
+        moment.locale('mailru', {
+            months : [AJS.I18n.getText('ru.mail.jira.plugins.calendar.January'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.February'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.March'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.April'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.May'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.June'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.July'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.August'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.September'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.October'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.November'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.December')],
+            monthsShort : [AJS.I18n.getText('ru.mail.jira.plugins.calendar.Jan'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Feb'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Mar'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Apr'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.May'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Jun'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Jul'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Aug'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Sep'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Oct'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Nov'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Dec')],
+            weekdays : [AJS.I18n.getText('ru.mail.jira.plugins.calendar.Sunday'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Monday'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Tuesday'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Wednesday'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Thursday'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Friday'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Saturday')],
+            weekdaysShort : [AJS.I18n.getText('ru.mail.jira.plugins.calendar.Sun'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Mon'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Tue'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Wed'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Thu'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Fri'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Sat')],
+            weekdaysMin : [AJS.I18n.getText('ru.mail.jira.plugins.calendar.Sun'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Mon'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Tue'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Wed'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Thu'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Fri'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Sat')],
+        });
+
         TimelineView = View.extend({
             defaultInterval: moment.duration(7, 'd'),
             timelineOptions: {
-                animateZoom: false,
-                minHeight: 400,
+                minHeight: 550,
+                multiselect: false,
+                throttleRedraw: 100,
                 zoomable: false,
                 zoomMin: moment.duration(4, 'h').asMilliseconds(),
-                zoomMax: moment.duration(6, 'M').asMilliseconds(),
-                'MONTHS': [AJS.I18n.getText('ru.mail.jira.plugins.calendar.January'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.February'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.March'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.April'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.May'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.June'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.July'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.August'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.September'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.October'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.November'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.December')],
-                'MONTHS_SHORT': [AJS.I18n.getText('ru.mail.jira.plugins.calendar.Jan'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Feb'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Mar'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Apr'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.May'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Jun'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Jul'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Aug'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Sep'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Oct'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Nov'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Dec')],
-                'DAYS': [AJS.I18n.getText('ru.mail.jira.plugins.calendar.Sunday'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Monday'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Tuesday'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Wednesday'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Thursday'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Friday'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Saturday')],
-                'DAYS_SHORT': [AJS.I18n.getText('ru.mail.jira.plugins.calendar.Sun'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Mon'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Tue'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Wed'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Thu'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Fri'), AJS.I18n.getText('ru.mail.jira.plugins.calendar.Sat')],
+                zoomMax: moment.duration(6, 'M').asMilliseconds()
             },
             initialize: function() {
             },
             _destroy: function() {
                 this.eventDialog = undefined;
-                this.timeline.deleteAllItems()
+                this.timeline.off('select');
+                this.timeline.off('rangechanged');
+                this.timeline.off('rangechange');
                 this.timeline = undefined;
-            },
-            getSelectedEvent: function() {
-                var sel = this.timeline.getSelection();
-                var row = sel.length ? sel[0].row : undefined;
-                return this.events[row];
             },
             render: function() {
                 if (!this.timeline) {
-                    this.calendar.option('contentHeight', 600);
-
-                    this.timeline = new links.Timeline(this.el[0], this.timelineOptions);
+                    this.timeline = new vis.Timeline(this.el[0], null, $.extend(this.timelineOptions, {start: this.start, end: this.end}));
                     $(this.el).on('remove', $.proxy(this._destroy, this));
-                    links.events.addListener(this.timeline, 'select', $.proxy(this._onEventSelect, this));
-                    links.events.addListener(this.timeline, 'rangechanged', $.proxy(this._onRangeChanged, this));
-                    links.events.addListener(this.timeline, 'rangechange', $.proxy(this._onRangeChange, this));
+                    this.timeline.on('select', $.proxy(this._onEventSelect, this));
+                    this.timeline.on('rangechanged', $.proxy(this._onRangeChanged, this));
+                    this.timeline.on('rangechange', $.proxy(this._onRangeChange, this));
                     this.setRange();
                 }
             },
             renderEvents: function(_events) {
-                this.events = this._transformToTimelineFormat(_events);
-                this.timeline.draw(this.events, null);
+                var events = this._transformToTimelineFormat(_events);
+                this.timeline.setData({items: events})
             },
             getRangeInterval: function() {
-                var diff = 0;
+                var diff;
                 if(this.timeline) {
-                    var visibleRange = this.timeline.getVisibleChartRange();
+                    var visibleRange = this.timeline.getWindow();
                     diff = visibleRange.end.getTime() - visibleRange.start.getTime();
                 }
-                return diff < this.defaultInterval.asMilliseconds() ? this.defaultInterval.asMilliseconds() : diff;
+                return diff || this.defaultInterval.asMilliseconds();
             },
             getVisibleRange: function() {
                 if(this.timeline)
-                    return this.timeline.getVisibleChartRange();
+                    return this.timeline.getWindow();
                 return {
                     start: moment().subtract(this.defaultInterval).toDate(),
                     end: moment().add(this.defaultInterval).toDate()
@@ -99,55 +99,58 @@
                     intervalEnd: intervalEnd
                 };
             },
+            setDate: function(date) {
+                if (this.timeline)
+                    this.timeline.moveTo(date);
+                else
+                    View.prototype.setDate.call(this, date);
+            },
             setRange: function(range) {
-                if (!range) {
-                    range = this.computeRange();
-                } else if (this.timeline)
-                    this.timeline.setVisibleChartRange(range.start.toDate(), range.end.toDate());
-
-                this.intervalDuration = moment.duration(range.end.toDate().getTime() - range.start.toDate().getTime());
+                range = range || this.computeRange();
+                this.intervalDuration = moment.duration(range.intervalEnd.toDate().getTime() - range.intervalStart.toDate().getTime());
                 View.prototype.setRange.call(this, range);
             },
             zoomOut: function() {
-                this.timeline.zoom(-0.3);
-                this._onRangeChanged();
+                this.timeline.range.zoom(1.3);
             },
             zoomIn: function() {
-                this.timeline.zoom(0.3);
-                this._onRangeChanged();
+                this.timeline.range.zoom(0.7);
             },
 
             _onRangeChange: function() {
-                this.timeline.setSelection([]);
                 this.eventDialog && this.eventDialog.hide();
             },
             _onRangeChanged: function() {
+                var _start = this.start;
+                var _end = this.end;
                 this.setRange();
                 this.calendar.date = this.massageCurrentDate(this.calendar.date);
-                this.display(this.calendar.date);
-                this.calendar.unfreezeContentHeight();
                 this.calendar.updateHeaderTitle();
                 this.calendar.updateTodayButton();
-                this.calendar.getAndRenderEvents();
+                if(_start != this.start || _end != this.end) {
+                    this.display(this.calendar.date);
+                    this.calendar.unfreezeContentHeight();
+                    this.calendar.getAndRenderEvents();
+                }
             },
-            _onEventSelect: function() {
-                var event = this.getSelectedEvent();
-                var target = $(".timeline-event-selected")
-                if (!event || event === this.currentSelectedEvent) {
+            _onEventSelect: function(e) {
+                var event = this.timeline.itemsData.get(e.items[0]);
+                if (!event || !e.items.length || (this.currentSelectedEvent && event.id == this.currentSelectedEvent.id)) {
                     this.eventDialog && this.eventDialog.hide();
                     return;
                 }
+                var target = e.event.target;
                 this.currentSelectedEvent = event;
                 this.eventDialog = AJS.InlineDialog(target, "eventDialog", function(content, trigger, showPopup) {
                     $.ajax({
                         type: 'GET',
-                        url: AJS.contextPath() + '/rest/mailrucalendar/1.0/calendar/events/' + event.calendarId + '/event/' + event.id + '/info',
+                        url: AJS.contextPath() + '/rest/mailrucalendar/1.0/calendar/events/' + event.calendarId + '/event/' + event.eventId + '/info',
                         success: function(issue) {
                             content.html(issueInfoTpl({issue: issue})).addClass('calendar-event-info-popup');
                             showPopup();
                         },
                         error: function(xhr) {
-                            var msg = "Error while trying to view info about issue => " + event.id;
+                            var msg = "Error while trying to view info about issue => " + event.eventId;
                             if (xhr.responseText)
                                 msg += xhr.responseText;
                             alert(msg);
@@ -166,12 +169,13 @@
             _transformToTimelineFormat: function(events) {
                 return _.map(events, function(event) {
                     return {
-                        id: event.id,
+                        id: event.calendarId + event.id,
+                        eventId: event.id,
                         calendarId: event.calendarId,
                         start: event.start.toDate(),
                         end: event.end && event.end.toDate(),
                         content: event.title,
-                        className: event.color
+                        style: AJS.format('background-color: {0};background: {0};border-color: {0};', event.color)
                     };
                 });
             }
