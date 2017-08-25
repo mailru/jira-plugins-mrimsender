b90a24f03fd6edf8385249fe75e41c6454c1ced2
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2017-08-25 14:57:24 +0300

    revert change

--- src/main/java/ru/mail/jira/plugins/calendar/service/recurrent/validation/CronRecurrenceValidator.java
+++ src/main/java/ru/mail/jira/plugins/calendar/service/recurrent/validation/CronRecurrenceValidator.java
@@ -1,18 +1,16 @@
 package ru.mail.jira.plugins.calendar.service.recurrent.validation;
 
 import com.atlassian.sal.api.message.I18nResolver;
-import org.quartz.CronExpression;
+import org.springframework.scheduling.support.CronSequenceGenerator;
 import ru.mail.jira.plugins.calendar.rest.dto.CustomEventDto;
 import ru.mail.jira.plugins.commons.RestFieldException;
-
-import java.text.ParseException;
 
 public class CronRecurrenceValidator implements RecurrenceValidator {
     @Override
     public void validateDto(I18nResolver i18nResolver, CustomEventDto customEventDto) {
         try {
-            CronExpression.validateExpression(customEventDto.getRecurrenceExpression()); //no way to validate otherwise in this version of spring
-        } catch (ParseException e) {
+            new CronSequenceGenerator(customEventDto.getRecurrenceExpression()); //no way to validate otherwise in this version of spring
+        } catch (IllegalArgumentException e) {
             throw new RestFieldException(i18nResolver.getText("ru.mail.jira.plugins.calendar.customEvents.recurrence.error.invalidCronExpression", e.getMessage()), "recurrenceExpression");
         }
 
