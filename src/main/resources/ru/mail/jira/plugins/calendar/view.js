cd8e2d2a15da108688c094cd062aef6df72af7d5
Alexey Belostotskiy <a.belostotskiy@corp.mail.ru>
2018-10-18 12:06:45 +0300

    remove top.mail.ru counter

--- src/main/resources/ru/mail/jira/plugins/calendar/view.js
+++ src/main/resources/ru/mail/jira/plugins/calendar/view.js
@@ -38,8 +38,6 @@ require(['jquery',
     });
 
     AJS.toInit(function() {
-        collectTopMailCounterScript();
-
         /* Models and Collections*/
         var UserData = Backbone.Model.extend({url: AJS.contextPath() + '/rest/mailrucalendar/1.0/calendar/userPreference'});
         var Calendar = Backbone.Model.extend();
@@ -638,30 +636,3 @@ require(['jquery',
         });
     });
 });
-
-/**
- * Run statistic counter - like Google Analytics.
- * Surely, it doesn't collect any personal data or private information.
- * All information you can check on top.mail.ru.
- */
-function collectTopMailCounterScript() {
-    var _tmr = window._tmr || (window._tmr = []);
-    _tmr.push({id: "2706504", type: "pageView", start: (new Date()).getTime()});
-    (function(d, w, id) {
-        if (d.getElementById(id)) return;
-        var ts = d.createElement("script");
-        ts.type = "text/javascript";
-        ts.async = true;
-        ts.id = id;
-        ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
-        var f = function() {
-            var s = d.getElementsByTagName("script")[0];
-            s.parentNode.insertBefore(ts, s);
-        };
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else {
-            f();
-        }
-    })(document, window, "topmailru-code");
-}
