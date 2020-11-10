57afa68cba4ebe635a5928dd60b7afc363b9d955
Raschudesny <rifkinsm@gmail.com>
2020-11-10 19:47:57 +0300

    unnecessary sentry client initialization removed, unused http async client dependency deleted

--- src/main/java/ru/mail/jira/plugins/myteam/configuration/ThirdPartyServiceInitializer.java
+++ src/main/java/ru/mail/jira/plugins/myteam/configuration/ThirdPartyServiceInitializer.java
@@ -7,25 +7,15 @@ import com.mashape.unirest.http.ObjectMapper;
 import com.mashape.unirest.http.Unirest;
 import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import ru.mail.jira.plugins.commons.SentryClient;
 
 @Component
 @Slf4j
 @ExportAsService(LifecycleAware.class)
 public class ThirdPartyServiceInitializer implements LifecycleAware {
-  private static final String DSN =
-      "http://488a7fa73b2343afb523a7139a7226fa:08f1454a8d514306a3cd2837cfc080a3@sentry.intdev.devmail.ru/19";
-
-  @Autowired
-  public ThirdPartyServiceInitializer() {}
 
   @Override
   public void onStart() {
-    log.info("Init SentryClient");
-    SentryClient.init(DSN);
-
     log.info("Init Unirest");
     Unirest.setTimeouts(10_000, 300_000);
     Unirest.setObjectMapper(
@@ -64,9 +54,6 @@ public class ThirdPartyServiceInitializer implements LifecycleAware {
 
   @Override
   public void onStop() {
-    log.info("Stop SentryClient");
-    SentryClient.close();
-
     try {
       log.info("Stop Unirest");
       Unirest.shutdown();
