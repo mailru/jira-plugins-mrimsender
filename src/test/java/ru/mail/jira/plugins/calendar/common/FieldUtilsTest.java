12c6b4b5ea1e57ebd2b1e08504b044bfdeb71048
Ivan Mashintsev <i.mashintsev@corp.mail.ru>
2021-02-12 16:18:46 +0300

    dummy test

---
+++ src/test/java/ru/mail/jira/plugins/calendar/common/FieldUtilsTest.java
@@ -0,0 +1,17 @@
+package ru.mail.jira.plugins.calendar.common;
+
+import static org.junit.jupiter.api.Assertions.*;
+class FieldUtilsTest {
+
+    @org.junit.jupiter.api.Test
+    void getSearcherKey() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isDateField() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isScriptRunnerField() {
+    }
+}
