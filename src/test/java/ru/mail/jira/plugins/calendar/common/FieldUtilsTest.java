d0d631e31a0b5d9130b084fa0862f3be2ebeeb58
Ivan Mashintsev <i.mashintsev@corp.mail.ru>
2021-02-12 16:22:34 +0300

    add jupiter engine

--- src/test/java/ru/mail/jira/plugins/calendar/common/FieldUtilsTest.java
+++ src/test/java/ru/mail/jira/plugins/calendar/common/FieldUtilsTest.java
@@ -1,7 +1,10 @@
 package ru.mail.jira.plugins.calendar.common;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class FieldUtilsTest {
+
+public class FieldUtilsTest {
 
     @org.junit.jupiter.api.Test
     void getSearcherKey() {
