26b8b9fcc2b52b5661bfda29156a409b04156ae3
Daria Rostshina <daria.rostshina@corp.mail.ru>
2021-11-18 11:47:16 +0300

    Added sending message to user group function JD-1801

--- src/main/java/ru/mail/jira/plugins/myteam/protocol/MyteamServiceImpl.java
+++ src/main/java/ru/mail/jira/plugins/myteam/protocol/MyteamServiceImpl.java
@@ -1,7 +1,10 @@
 /* (C)2020 */
 package ru.mail.jira.plugins.myteam.protocol;
 
+import com.atlassian.crowd.embedded.api.Group;
+import com.atlassian.jira.security.groups.GroupManager;
 import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,11 +14,16 @@ import ru.mail.jira.plugins.myteam.protocol.listeners.MyteamEventsListener;
 
 @Service
 public class MyteamServiceImpl implements MyteamService {
+  private final GroupManager groupManager;
   private final UserData userData;
   private final MyteamEventsListener myteamEventsListener;
 
   @Autowired
-  public MyteamServiceImpl(MyteamEventsListener myteamEventsListener, UserData userData) {
+  public MyteamServiceImpl(
+      @ComponentImport GroupManager groupManager,
+      MyteamEventsListener myteamEventsListener,
+      UserData userData) {
+    this.groupManager = groupManager;
     this.userData = userData;
     this.myteamEventsListener = myteamEventsListener;
   }
@@ -31,6 +39,21 @@ public class MyteamServiceImpl implements MyteamService {
       return true;
     }
     return false;
+  }
+
+  @Override
+  public void sendMessageToUserGroup(String groupName, String message) {
+    if (groupName == null || StringUtils.isEmpty(message))
+      throw new IllegalArgumentException("Group name and message must be specified");
+
+    Group group = groupManager.getGroup(groupName);
+    if (group == null)
+      throw new IllegalArgumentException(
+          String.format("Group with name %s does not exist", groupName));
+
+    for (ApplicationUser user : groupManager.getUsersInGroup(group)) {
+      sendMessage(user, message);
+    }
   }
 
   @Override
