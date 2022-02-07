6821091b19e5b759b6a584fee014148c3e8dedd5
Udalov Nikita <n.udalov@corp.mail.ru>
2022-02-07 21:03:50 +0300

    add access for jira admins

--- src/main/java/ru/mail/jira/plugins/myteam/actions/ChatSettingsAction.java
+++ src/main/java/ru/mail/jira/plugins/myteam/actions/ChatSettingsAction.java
@@ -1,6 +1,8 @@
 /* (C)2022 */
 package ru.mail.jira.plugins.myteam.actions;
 
+import com.atlassian.jira.permission.GlobalPermissionKey;
+import com.atlassian.jira.security.GlobalPermissionManager;
 import com.atlassian.jira.security.JiraAuthenticationContext;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.web.action.JiraWebActionSupport;
@@ -16,19 +18,26 @@ public class ChatSettingsAction extends JiraWebActionSupport {
   private static final Pattern pattern = Pattern.compile("chatId=([^&]+)");
   private final MyteamApiClient myteamClient;
   private final JiraAuthenticationContext jiraAuthenticationContext;
+  private final GlobalPermissionManager globalPermissionManager;
 
   public ChatSettingsAction(
       MyteamApiClient myteamClient,
-      @ComponentImport JiraAuthenticationContext jiraAuthenticationContext) {
+      @ComponentImport JiraAuthenticationContext jiraAuthenticationContext,
+      @ComponentImport GlobalPermissionManager globalPermissionManager) {
     this.myteamClient = myteamClient;
     this.jiraAuthenticationContext = jiraAuthenticationContext;
+    this.globalPermissionManager = globalPermissionManager;
   }
 
   @Override
   public String execute() {
+    ApplicationUser user = jiraAuthenticationContext.getLoggedInUser();
+
+    if (isJiraAdmin(user)) {
+      return SUCCESS;
+    }
     String query = this.getHttpRequest().getQueryString();
     Matcher matcher = pattern.matcher(query);
-    ApplicationUser user = jiraAuthenticationContext.getLoggedInUser();
 
     if (matcher.find()) {
       if (!checkPermissions(matcher.group(1), user.getEmailAddress())) return SECURITY_BREACH;
@@ -44,5 +53,9 @@ public class ChatSettingsAction extends JiraWebActionSupport {
       log.error("Unable to get chat admins", e);
       return false;
     }
+  }
+
+  private boolean isJiraAdmin(ApplicationUser user) {
+    return globalPermissionManager.hasPermission(GlobalPermissionKey.ADMINISTER, user);
   }
 }
