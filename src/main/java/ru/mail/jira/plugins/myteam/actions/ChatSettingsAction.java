48f88d8f230c12d60fd6ffde6433f7ba5bd0dc7d
Udalov Nikita <n.udalov@corp.mail.ru>
2022-02-10 00:48:08 +0300

    Security check

--- src/main/java/ru/mail/jira/plugins/myteam/actions/ChatSettingsAction.java
+++ src/main/java/ru/mail/jira/plugins/myteam/actions/ChatSettingsAction.java
@@ -9,24 +9,22 @@ import com.atlassian.jira.web.action.JiraWebActionSupport;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import ru.mail.jira.plugins.myteam.exceptions.MyteamServerErrorException;
-import ru.mail.jira.plugins.myteam.myteam.MyteamApiClient;
-import ru.mail.jira.plugins.myteam.myteam.dto.response.AdminsResponse;
+import ru.mail.jira.plugins.myteam.rulesengine.service.UserChatService;
 
 public class ChatSettingsAction extends JiraWebActionSupport {
   private static final String SECURITY_BREACH = "securitybreach";
   private static final Pattern pattern = Pattern.compile("chatId=([^&]+)");
-  private final MyteamApiClient myteamClient;
   private final JiraAuthenticationContext jiraAuthenticationContext;
   private final GlobalPermissionManager globalPermissionManager;
+  private final UserChatService userChatService;
 
   public ChatSettingsAction(
-      MyteamApiClient myteamClient,
       @ComponentImport JiraAuthenticationContext jiraAuthenticationContext,
-      @ComponentImport GlobalPermissionManager globalPermissionManager) {
-    this.myteamClient = myteamClient;
+      @ComponentImport GlobalPermissionManager globalPermissionManager,
+      UserChatService userChatService) {
     this.jiraAuthenticationContext = jiraAuthenticationContext;
     this.globalPermissionManager = globalPermissionManager;
+    this.userChatService = userChatService;
   }
 
   @Override
@@ -40,19 +38,10 @@ public class ChatSettingsAction extends JiraWebActionSupport {
     Matcher matcher = pattern.matcher(query);
 
     if (matcher.find()) {
-      if (!checkPermissions(matcher.group(1), user.getEmailAddress())) return SECURITY_BREACH;
+      if (!userChatService.isChatAdmin(matcher.group(1), user.getEmailAddress()))
+        return SECURITY_BREACH;
     }
     return SUCCESS;
-  }
-
-  private boolean checkPermissions(String chatId, String userId) throws SecurityException {
-    try {
-      AdminsResponse response = myteamClient.getAdmins(chatId).getBody();
-      return response.getAdmins().stream().anyMatch(admin -> admin.getUserId().equals(userId));
-    } catch (MyteamServerErrorException e) {
-      log.error("Unable to get chat admins", e);
-      return false;
-    }
   }
 
   private boolean isJiraAdmin(ApplicationUser user) {
--- src/main/java/ru/mail/jira/plugins/myteam/controller/IssueCreationSettingsController.java
+++ src/main/java/ru/mail/jira/plugins/myteam/controller/IssueCreationSettingsController.java
@@ -1,10 +1,20 @@
 /* (C)2022 */
 package ru.mail.jira.plugins.myteam.controller;
 
+import com.atlassian.jira.exception.PermissionException;
+import com.atlassian.jira.permission.GlobalPermissionKey;
+import com.atlassian.jira.security.GlobalPermissionManager;
+import com.atlassian.jira.security.JiraAuthenticationContext;
+import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import java.util.List;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.mail.jira.plugins.myteam.dto.IssueCreationSettingsDto;
+import ru.mail.jira.plugins.myteam.rulesengine.service.UserChatService;
 import ru.mail.jira.plugins.myteam.service.IssueCreationSettingsService;
 
 @Path("/issueCreation/settings")
@@ -12,10 +22,25 @@ import ru.mail.jira.plugins.myteam.service.IssueCreationSettingsService;
 public class IssueCreationSettingsController {
 
   private final IssueCreationSettingsService issueCreationSettingsService;
+  private final UserChatService userChatService;
+  private final JiraAuthenticationContext jiraAuthenticationContext;
+  private final GlobalPermissionManager globalPermissionManager;
+
+  @ResponseStatus(value = HttpStatus.FORBIDDEN)
+  @ExceptionHandler(PermissionException.class)
+  public String noRightsHandleException(PermissionException e) {
+    return e.getLocalizedMessage();
+  }
 
   public IssueCreationSettingsController(
-      IssueCreationSettingsService issueCreationSettingsService) {
+      IssueCreationSettingsService issueCreationSettingsService,
+      UserChatService userChatService,
+      @ComponentImport JiraAuthenticationContext jiraAuthenticationContext,
+      @ComponentImport GlobalPermissionManager globalPermissionManager) {
     this.issueCreationSettingsService = issueCreationSettingsService;
+    this.userChatService = userChatService;
+    this.jiraAuthenticationContext = jiraAuthenticationContext;
+    this.globalPermissionManager = globalPermissionManager;
   }
 
   @GET
@@ -26,7 +51,9 @@ public class IssueCreationSettingsController {
 
   @GET
   @Path("")
-  public IssueCreationSettingsDto getChatSettings(@QueryParam("chatId") final String chatId) {
+  public IssueCreationSettingsDto getChatSettings(@QueryParam("chatId") final String chatId)
+      throws PermissionException {
+    checkPermissions(chatId);
     return issueCreationSettingsService.getSettingsByChatId(chatId).orElse(null);
   }
 
@@ -34,7 +61,23 @@ public class IssueCreationSettingsController {
   @Path("/{id}")
   @Consumes(MediaType.APPLICATION_JSON)
   public IssueCreationSettingsDto updateChatSettings(
-      @PathParam("id") final int id, final IssueCreationSettingsDto settings) {
+      @PathParam("id") final int id, final IssueCreationSettingsDto settings)
+      throws PermissionException {
+    IssueCreationSettingsDto originalSettings = issueCreationSettingsService.getSettings(id);
+    checkPermissions(originalSettings.getChatId());
     return issueCreationSettingsService.updateSettings(id, settings);
+  }
+
+  private void checkPermissions(String chatId) throws PermissionException {
+    ApplicationUser user = jiraAuthenticationContext.getLoggedInUser();
+
+    if (isJiraAdmin(user) || userChatService.isChatAdmin(chatId, user.getEmailAddress())) {
+      return;
+    }
+    throw new PermissionException();
+  }
+
+  private boolean isJiraAdmin(ApplicationUser user) {
+    return globalPermissionManager.hasPermission(GlobalPermissionKey.ADMINISTER, user);
   }
 }
