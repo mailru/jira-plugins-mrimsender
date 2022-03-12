df9b7c81f84c9693d02b8599641f68b3abbbd5b3
Ivan Mashintsev <i.mashintsev@corp.mail.ru>
2022-03-12 17:39:12 +0300

    hotfix ProjectAdminAction

--- src/main/java/ru/mail/jira/plugins/myteam/actions/ProjectChatAdminAction.java
+++ src/main/java/ru/mail/jira/plugins/myteam/actions/ProjectChatAdminAction.java
@@ -1,17 +1,16 @@
 /* (C)2020 */
 package ru.mail.jira.plugins.myteam.actions;
 
-import com.atlassian.jira.bc.project.ProjectService;
 import com.atlassian.jira.web.action.JiraWebActionSupport;
 import org.apache.commons.lang3.StringUtils;
 import ru.mail.jira.plugins.myteam.commons.PermissionHelperService;
 
-public class ProjectAdminAction extends JiraWebActionSupport {
+public class ProjectChatAdminAction extends JiraWebActionSupport {
   private static final String SECURITY_BREACH = "securitybreach";
 
   private final PermissionHelperService permissionHelperService;
 
-  public ProjectAdminAction(PermissionHelperService permissionHelperService) {
+  public ProjectChatAdminAction(PermissionHelperService permissionHelperService) {
     this.permissionHelperService = permissionHelperService;
   }
 
