45af36d43785a28c092987d56e723d60cedbf99a
Udalov Nikita <n.udalov@corp.mail.ru>
2022-03-21 17:48:06 +0300

    removed unused variable

--- src/main/java/ru/mail/jira/plugins/myteam/rulesengine/rules/ChatAdminRule.java
+++ src/main/java/ru/mail/jira/plugins/myteam/rulesengine/rules/ChatAdminRule.java
@@ -1,13 +1,11 @@
 /* (C)2022 */
 package ru.mail.jira.plugins.myteam.rulesengine.rules;
 
-import lombok.extern.slf4j.Slf4j;
 import ru.mail.jira.plugins.myteam.protocol.events.MyteamEvent;
 import ru.mail.jira.plugins.myteam.rulesengine.models.exceptions.AdminRulesRequiredException;
 import ru.mail.jira.plugins.myteam.service.RulesEngine;
 import ru.mail.jira.plugins.myteam.service.UserChatService;
 
-@Slf4j
 public class ChatAdminRule extends BaseRule {
   public ChatAdminRule(UserChatService userChatService, RulesEngine rulesEngine) {
     super(userChatService, rulesEngine);
