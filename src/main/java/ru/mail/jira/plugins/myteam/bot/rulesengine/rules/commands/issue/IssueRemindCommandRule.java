01136ac459bc7249073eb82a5ebc2cd58673d0ba
Udalov Nikita <n.udalov@corp.mail.ru>
2023-03-29 13:36:09 +0300

    remind tomorrow date fix

--- src/main/java/ru/mail/jira/plugins/myteam/bot/rulesengine/rules/commands/issue/IssueRemindCommandRule.java
+++ src/main/java/ru/mail/jira/plugins/myteam/bot/rulesengine/rules/commands/issue/IssueRemindCommandRule.java
@@ -3,8 +3,8 @@ package ru.mail.jira.plugins.myteam.bot.rulesengine.rules.commands.issue;
 
 import com.atlassian.jira.user.ApplicationUser;
 import java.io.IOException;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 import org.jeasy.rules.annotation.Action;
@@ -51,23 +51,20 @@ public class IssueRemindCommandRule extends BaseRule {
 
     rulesEngine.fireError(ErrorRuleType.UnknownError, event);
 
-    if (parsedArgs.size() < 2 || user == null) {
+    if (parsedArgs.size() < 1 || user == null) {
       rulesEngine.fireError(ErrorRuleType.UnknownError, event);
       answerButtonCallback(event);
     }
 
     ReminderDto.ReminderDtoBuilder builder = ReminderDto.builder();
 
-    builder.date(
-        Date.from(
-            ZonedDateTime.parse(parsedArgs.get(1), DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-                .toInstant()));
+    builder.date(getDate24HoursAhead());
 
     builder.issueKey(parsedArgs.get(0));
     builder.userEmail(event.getUserId());
 
-    if (parsedArgs.size() == 3) {
-      builder.description(parsedArgs.get(2));
+    if (parsedArgs.size() == 2) {
+      builder.description(parsedArgs.get(1));
     }
 
     reminderService.addReminder(builder.build(), user);
@@ -78,5 +75,11 @@ public class IssueRemindCommandRule extends BaseRule {
             "ru.mail.jira.plugins.myteam.mrimsenderEventListener.remindTomorrow.success.text"));
 
     answerButtonCallback(event);
+  }
+
+  private static Date getDate24HoursAhead() {
+    LocalDateTime now = LocalDateTime.now(ZoneId.systemDefault());
+
+    return Date.from(now.plusHours(24).atZone(ZoneId.systemDefault()).toInstant());
   }
 }
