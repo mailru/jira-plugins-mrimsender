e745dd9c95d47b50ae6f0983e718f5bde8f200b7
t.abubakirov <t.abubakirov@vk.team>
2023-09-19 14:34:03 +0300

    JD-2880 added ignoring unknown properties in team event

--- src/main/java/ru/mail/jira/plugins/myteam/myteam/dto/TextFormatMetadata.java
+++ src/main/java/ru/mail/jira/plugins/myteam/myteam/dto/TextFormatMetadata.java
@@ -3,13 +3,16 @@ package ru.mail.jira.plugins.myteam.myteam.dto;
 
 import java.util.List;
 import lombok.Data;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 @SuppressWarnings("NullAway")
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TextFormatMetadata {
   private List<Link> link;
 
   @SuppressWarnings("NullAway")
+  @JsonIgnoreProperties(ignoreUnknown = true)
   @Data
   public static final class Link {
     private int length;
