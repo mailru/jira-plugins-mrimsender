cd2ce9dcd4da04983d697747d654fade50f66d5a
t.abubakirov <t.abubakirov@vk.team>
2024-02-08 12:45:46 +0300

    JD-3044 added new field to dto and annotations; added tests;

--- src/main/java/ru/mail/jira/plugins/myteam/myteam/dto/chats/Member.java
+++ src/main/java/ru/mail/jira/plugins/myteam/myteam/dto/chats/Member.java
@@ -1,8 +1,18 @@
 /* (C)2021 */
 package ru.mail.jira.plugins.myteam.myteam.dto.chats;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
 @SuppressWarnings("NullAway")
+@Getter
+@Setter
+@ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Member {
   public String userId;
   public boolean creator;
+  public boolean admin;
 }
